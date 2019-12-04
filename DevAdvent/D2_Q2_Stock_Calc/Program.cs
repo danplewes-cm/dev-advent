966369df288e4485d58f917b0e23d4c4949ad703
dan.plewes <dan.plewes@cerebrahealth.com>
2019-12-04 15:38:22 -0600

    Added Q2 for day 2

---
+++ DevAdvent/D2_Q2_Stock_Calc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D2_Q2_Stock_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        List<int> input = DataGenerator.DataGeneratorUtils.FetchData(0, 10000,100);
+
+			StockPicker sp = new StockPicker(input);
+	        StockPicker.StockResult sr = sp.DetermineMaxProfit();
+
+			Console.WriteLine($"Data: [{string.Join(",", input)}]");
+	        if (sr != null)
+	        {
+				Console.WriteLine(sr);
+	        }
+			
+			Console.WriteLine("Done");
+	        Console.Read();
+        }
+    }
+}
---
+++ DevAdvent/D2_Q2_Stock_Calc/StockPicker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D2_Q2_Stock_Calc
+{
+    class StockPicker
+    {
+	    public class StockResult
+	    {
+		    public int Profit => SellPrice - BuyPrice;
+		    public int BuyPrice;
+		    public int SellPrice;
+			
+		    public StockResult(int buyPrice, int sellPrice)
+		    {
+			    BuyPrice = buyPrice;
+			    SellPrice = sellPrice;
+		    }
+
+		    public override string ToString()
+		    {
+			    return $"Largest profit is {Profit}, [{BuyPrice},{SellPrice}]";
+		    }
+	    }
+
+		private readonly List<int> _inputData;
+
+	    public StockPicker(List<int> inputData)
+	    {
+		    _inputData = inputData;
+	    }
+
+	    public StockResult DetermineMaxProfit()
+	    {
+		    int currSell = -1;
+		    int currBuy = _inputData[0];
+
+			StockResult bestResult = null;
+
+		    for (int i = 1; i < _inputData.Count; i++)
+		    {
+			    var currVal = _inputData[i];
+			    if (currSell < 0)
+			    {
+				    if (currVal < currBuy)
+				    {
+					    currBuy = currVal;
+				    }
+				    else
+				    {
+					    currSell = currVal;
+					    bestResult = new StockResult(currBuy, currSell);
+				    }
+			    }
+			    else
+			    {
+				    if (currVal > currSell)
+				    {
+					    currSell = currVal;
+				    }
+
+				    if (currVal < currBuy)
+				    {
+					    currBuy = currVal;
+					    currSell = -1;
+				    }
+			    }
+
+			    if (bestResult == null || currSell - currBuy < bestResult.Profit)
+			    {
+					bestResult = new StockResult(currBuy, currSell);
+			    }
+				else if (currSell > bestResult.SellPrice)
+			    {
+				    bestResult.SellPrice = currSell;
+			    }
+		    }
+
+		    return bestResult;
+	    }
+	}
+}
