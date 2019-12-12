eec8d450777127ec2f595674628bb6c50b1a36d5
dan.plewes <dan.plewes@cerebrahealth.com>
2019-12-12 15:31:44 -0600

    Questions from day 4

---
+++ DevAdvent/D4_Q1_Contiguous_Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D4_Q1_Contiguous_Sum
+{
+    class Program
+    {
+		public static void Main()
+	    {
+		    Process(new List<int> { 15, 16, -66, 17, 18 });
+		    Process(new List<int> { 34, -50, 42, 14, -5, 86, -50, -50, -38 });
+		    Process(new List<int> { 34, -50, 42, 14, -5, 86 });
+		    Process(new List<int> { -5, -1, -8, -9 });
+
+		    Console.Read();
+	    }
+
+	    private static void Process(List<int> input)
+	    {
+		    int maxSum = FindMaxSum(input);
+
+		    Console.WriteLine(string.Join(",", input));
+		    Console.WriteLine("Max Sum: " + maxSum);
+	    }
+
+	    private static int FindMaxSum(List<int> inputData)
+	    {
+		    if (inputData.Count == 0)
+			    return 0;
+
+		    int currTestSum = 0;
+		    int bestSum = int.MinValue;
+
+		    for (int i = 0; i < inputData.Count; i++)
+		    {
+			    currTestSum += inputData[i];
+
+			    if (currTestSum > bestSum)
+				    bestSum = currTestSum;
+
+			    if (currTestSum < 0)
+				    currTestSum = 0;
+		    }
+
+		    if (bestSum < 0)
+			    bestSum = 0;
+
+		    return bestSum;
+	    }
+	}
+}
---
+++ DevAdvent/D4_Q2_Perms/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D4_Q2_Perms
+{
+    class Program
+    {
+	    public static void Main()
+	    {
+		    Process(new List<int>() { 1, 2, 3, 4, 5 });
+
+		    Console.Read();
+	    }
+
+	    private static void Process(List<int> input)
+	    {
+		    List<List<int>> perms = GetPermutations(input);
+
+		    foreach (var perm in perms)
+		    {
+			    Console.WriteLine("[" + string.Join(",", perm) + "]");
+		    }
+	    }
+
+	    public static List<List<int>> GetPermutations(List<int> input, int startIndex = 0)
+	    {
+		    List<List<int>> output = new List<List<int>>();
+
+		    if (startIndex == input.Count - 1)
+		    {
+			    output.Add(new List<int>(input));
+		    }
+		    else
+		    {
+			    for (int i = startIndex; i < input.Count; i++)
+			    {
+				    int temp = input[startIndex];
+				    input[startIndex] = input[i];
+				    input[i] = temp;
+
+				    output.AddRange(GetPermutations(input, startIndex + 1));
+			    }
+		    }
+
+		    return output;
+	    }
+	}
+}
